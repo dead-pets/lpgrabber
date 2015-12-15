738211b4a5bc12b0ceec46b4bc21e06bbf6beab3
Dmitry Pyzhov <dpyzhov@mirantis.com>
2015-12-15 16:35:10 +0300

    Fix ipython reports dependency

--- reports/build_reports.sh
+++ reports/build_reports.sh
@@ -3,6 +3,6 @@
 source_dir=${1:-.}
 target_dir=${2:-.}
 
-pip install ipython nbconvert jupyter-client jupyter-core ipykernel
+pip install ipython nbconvert jupyter-client jupyter-core ipykernel matplotlib
 
-cd $target_dir & ipython nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.allow_errors=True $source_dir/*.ipynb
+cd $target_dir && ipython nbconvert --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.allow_errors=True $source_dir/*.ipynb
